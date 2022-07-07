b07d96dd695568b94a95a4e6d5e712f41b37bbfe
Brian Lawhon <brianlawhon@live.com>
2022-07-07 09:34:26 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { React, Fragment } from 'react';
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
